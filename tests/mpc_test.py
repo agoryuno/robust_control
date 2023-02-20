a88641727b14d6eae7d30033f5c4035d274ec392
Alexander Goryunov <alex.goryunov@gmail.com>
2023-02-20 13:56:55 +0200

    Cleaned up mpc_test.py

--- tests/mpc_test.py
+++ tests/mpc_test.py
@@ -9,26 +9,24 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from robust_control import get_controls, split
 
 
-with open("price_mat.pkl", "rb") as f:
-    price_mat, _, _ = pickle.load(f)
+def make_arguments(batch_size=20, eta_n=10, mu_n=3):
+    with open("price_mat.pkl", "rb") as f:
+        price_mat, _, _ = pickle.load(f)
 
-print (os.cpu_count())        
+    row_batches = split([i for i in range(price_mat.shape[0])], price_mat.shape[0]//20)
+    return [[price_mat, rows, eta_n, mu_n] for rows in row_batches]
 
-eta_n = 10
-mu_n = 3
-i = 0
-row_batches = split([i for i in range(price_mat.shape[0])], price_mat.shape[0]//10)
 
 def fn(*args):
     price_mat, rows, eta_n, mu_n = args
     return get_controls(price_mat, rows, eta_n, mu_n=mu_n, cuda=False, 
                 parts=False, preint=90, train=0.79, double=True)
 
-args = [[price_mat, rows, eta_n, mu_n] for rows in row_batches]
-
-def run():
+def run(args):
     with Pool() as p:
         p.starmap(fn, args)
 
+
 if __name__ == "__main__":
-    run()
+    args = make_arguments()
+    run(args)
