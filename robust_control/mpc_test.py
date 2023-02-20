7aec5101fa79adc0d7501f32c520dc4aa4db9ce5
Alexander Goryunov <alex.goryunov@gmail.com>
2023-02-20 14:32:02 +0200

    Moved mpc_test.py to main package

---
+++ robust_control/mpc_test.py
@@ -0,0 +1,40 @@
+import pickle
+import os
+import sys
+from time import time
+from multiprocessing import Pool
+
+# add the parent directory to the path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from robust_control import get_controls, split
+
+
+def make_arguments(batch_size=20, eta_n=10, mu_n=3, preint=30,
+                   time_len=45):
+    
+    assert preint < time_len
+    with open("price_mat.pkl", "rb") as f:
+        price_mat, _, _ = pickle.load(f)
+
+    row_batches = split([i for i in range(price_mat[:, :time_len].shape[0])], price_mat.shape[0]//batch_size)
+    return [[price_mat, rows, eta_n, mu_n, preint] for rows in row_batches]
+
+
+def fn(*args):
+    price_mat, rows, eta_n, mu_n, preint = args
+    return get_controls(price_mat, rows, eta_n, mu_n=mu_n, cuda=False, 
+                parts=False, preint=preint, train=0.8, double=False)
+
+
+def run(args, procs=4):
+    with Pool(procs) as p:
+        p.starmap(fn, args)
+
+
+if __name__ == "__main__":
+    
+    args = make_arguments(30)
+    start = time()
+    run(args, procs=4)
+    print (time() - start)
