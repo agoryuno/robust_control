b20311b8e4ebbaeeffcfd660edbe9078aebcbc79
Alexander Goryunov <alex.goryunov@gmail.com>
2023-02-20 15:55:23 +0200

    Added arguments to mpc_test

--- tests/mpc_test.py
+++ tests/mpc_test.py
@@ -11,20 +11,21 @@ from robust_control import get_controls, split
 
 
 def make_arguments(batch_size=20, eta_n=10, mu_n=3, preint=30,
-                   time_len=45):
+                   time_len=45, train=0.8, double=False, cuda=False,
+                   parts=False):
     
     assert preint < time_len
     with open("price_mat.pkl", "rb") as f:
         price_mat, _, _ = pickle.load(f)
 
     row_batches = split([i for i in range(price_mat[:, :time_len].shape[0])], price_mat.shape[0]//batch_size)
-    return [[price_mat, rows, eta_n, mu_n, preint] for rows in row_batches]
+    return [[price_mat, rows, eta_n, mu_n, preint, train, double, cuda, parts] for rows in row_batches]
 
 
 def fn(*args):
-    price_mat, rows, eta_n, mu_n, preint = args
-    return get_controls(price_mat, rows, eta_n, mu_n=mu_n, cuda=False, 
-                parts=False, preint=preint, train=0.8, double=False)
+    price_mat, rows, eta_n, mu_n, preint, train, double, cuda, parts = args
+    return get_controls(price_mat, rows, eta_n, mu_n=mu_n, cuda=cuda, 
+                parts=parts, preint=preint, train=train, double=double)
 
 
 def run(args, procs=4):
