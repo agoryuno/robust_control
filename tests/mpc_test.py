09d941ebbf59659ce7cfcf051ec60402c35cf111
Alexander Goryunov <alex.goryunov@gmail.com>
2023-02-20 14:07:37 +0200

    .

--- tests/mpc_test.py
+++ tests/mpc_test.py
@@ -14,7 +14,7 @@ def make_arguments(batch_size=20, eta_n=10, mu_n=3):
     with open("price_mat.pkl", "rb") as f:
         price_mat, _, _ = pickle.load(f)
 
-    row_batches = split([i for i in range(price_mat.shape[0])], price_mat.shape[0]//20)
+    row_batches = split([i for i in range(price_mat.shape[0])], price_mat.shape[0]//batch_size)
     return [[price_mat, rows, eta_n, mu_n] for rows in row_batches]
 
 
@@ -23,14 +23,14 @@ def fn(*args):
     return get_controls(price_mat, rows, eta_n, mu_n=mu_n, cuda=False, 
                 parts=False, preint=90, train=0.79, double=True)
 
-def run(args):
-    with Pool() as p:
+def run(args, procs=4):
+    with Pool(procs) as p:
         p.starmap(fn, args)
 
 
 if __name__ == "__main__":
     
-    args = make_arguments()
+    args = make_arguments(15)
     start = time()
-    run(args)
+    run(args, procs=4)
     print (time() - start)
